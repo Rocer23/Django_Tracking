835cdb27d613b1fcd50bfa92ea7a1ba3207a7508
Rocer23 <ip314338@gmail.com>
2025-02-15 14:38:35 +0200

    Initial DataBase

--- tracking_system/models.py
+++ tracking_system/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    description = models.TextField()
+    status = models.BooleanField()
+    prioritet = models.IntegerField()
+    permission = models.BooleanField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['start']
+
+
+class Comments(models.Model):
+    text = models.TextField()
+    likes = models.IntegerField()
+    dislikes = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.text}'
